b19a71258c284c3173112cc07f928570a79f1d99
merlinjones10 <merlinjones10@gmail.com>
2021-10-27 14:59:05 +0100

    added numbers and calculation

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/Article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
 
 mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
-})
+  const articles = await Article.find().sort({ createdAt: 'desc' });
+  const totalObj = articles.reduce((a, b) => ({ number: a.number + b.number }));
+  const total = totalObj.number;
+  console.log(total);
+  res.render('articles/index', { articles: articles, total: total });
+});
 
-app.use('/articles', articleRouter)
+app.use('/articles', articleRouter);
 
-app.listen(5000)
+app.listen(5000);
