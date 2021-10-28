14d6ed48f1900cd3892e579d943094f21d0a1d8e
merlinjones10 <merlinjones10@gmail.com>
2021-10-28 12:52:14 +0100

    connected to remote DB and fixed errors

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const Article = require('./models/Article');
 const articleRouter = require('./routes/articles');
 const methodOverride = require('method-override');
+require('dotenv').config();
 const app = express();
+// 'mongodb://localhost/blog' <- previous db to local
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log('connected to DB!');
 });
 
 app.set('view engine', 'ejs');
@@ -16,11 +16,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' });
-  const totalObj = articles.reduce((a, b) => ({ number: a.number + b.number }));
-  const total = totalObj.number;
-  console.log(total);
-  res.render('articles/index', { articles: articles, total: total });
+  let total = 0;
+  try {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
+    if (articles.length) {
+      const totalObj = articles.reduce((a, b) => ({ number: a.number + b.number }));
+      total = totalObj.number;
+    }
+    res.render('articles/index', { articles: articles, total: total });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.use('/articles', articleRouter);
